b7e87242eab1cff553a75263f559928a493e1027
Harrison Goldstein <hgoldstein95@gmail.com>
2017-06-05 22:31:34 -0400

    Added lots

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-;; -*- mode: emacs-lisp -*-
 (defun dotspacemacs/layers ()
   (setq-default
    dotspacemacs-distribution 'spacemacs
@@ -7,6 +6,14 @@
    dotspacemacs-configuration-layer-path '()
    dotspacemacs-configuration-layers
    '(
+     elm
+     yaml
+     sql
+     idris
+     (haskell :variables
+              haskell-process-type 'stack-ghci)
+     graphviz
+     octave
      auto-completion
      better-defaults
      common-lisp
@@ -58,6 +65,7 @@
    dotspacemacs-startup-buffer-responsive t
    dotspacemacs-scratch-mode 'text-mode
    dotspacemacs-themes '(flatland
+                         spacemacs-light
                          spacemacs-dark
                          material
                          material-light)
@@ -104,7 +112,7 @@
    dotspacemacs-line-numbers nil
    dotspacemacs-folding-method 'evil
    dotspacemacs-smartparens-strict-mode nil
-   dotspacemacs-smart-closing-parenthesis t
+   dotspacemacs-smart-closing-parenthesis nil
    dotspacemacs-highlight-delimiters 'all
    dotspacemacs-persistent-server nil
    dotspacemacs-search-tools '("ag" "pt" "ack" "grep")
@@ -116,3 +124,69 @@
 
 (defun dotspacemacs/user-config ()
   (org-babel-load-file (concat dotspacemacs-directory "config.org")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(evil-want-Y-yank-to-eol t)
+ '(fci-rule-color "#202325")
+ '(safe-local-variable-values
+   (quote
+    ((idris-load-packages "base" "contrib")
+     (idris-load-packages quote
+                          ("base" "contrib")))))
+ '(vc-annotate-background "#1f2124")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#ff0000")
+     (40 . "#ff4a52")
+     (60 . "#f6aa11")
+     (80 . "#f1e94b")
+     (100 . "#f5f080")
+     (120 . "#f6f080")
+     (140 . "#41a83e")
+     (160 . "#40b83e")
+     (180 . "#b6d877")
+     (200 . "#b7d877")
+     (220 . "#b8d977")
+     (240 . "#b9d977")
+     (260 . "#93e0e3")
+     (280 . "#72aaca")
+     (300 . "#8996a8")
+     (320 . "#afc4db")
+     (340 . "#cfe2f2")
+     (360 . "#dc8cc3"))))
+ '(vc-annotate-very-old-color "#dc8cc3"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-latex-sectioning-0-face ((t (:height 1.0))))
+ '(font-latex-sectioning-1-face ((t (:height 1.0))))
+ '(font-latex-sectioning-2-face ((t (:height 1.0))))
+ '(font-latex-sectioning-3-face ((t (:height 1.0))))
+ '(font-latex-sectioning-4-face ((t (:height 1.0))))
+ '(font-latex-sectioning-5-face ((t (:height 1.0))))
+ '(font-latex-slide-title-face ((t (:height 1.0))))
+ '(info-title-1 ((t (:height 1.0))))
+ '(info-title-2 ((t (:height 1.0))))
+ '(info-title-3 ((t (:height 1.0))))
+ '(info-title-4 ((t (:height 1.0))))
+ '(markdown-header-face ((t (:height 1.0))))
+ '(markdown-header-face-1 ((t (:height 1.0))))
+ '(markdown-header-face-2 ((t (:height 1.0))))
+ '(markdown-header-face-3 ((t (:height 1.0))))
+ '(markdown-header-face-4 ((t (:height 1.0))))
+ '(markdown-header-face-5 ((t (:height 1.0))))
+ '(markdown-header-face-6 ((t (:height 1.0))))
+ '(org-document-title ((t (:height 1.0))))
+ '(org-level-1 ((t (:height 1.0))))
+ '(org-level-2 ((t (:height 1.0))))
+ '(org-level-3 ((t (:height 1.0))))
+ '(org-level-4 ((t (:height 1.0))))
+ '(org-level-5 ((t (:height 1.0))))
+ '(org-level-6 ((t (:height 1.0))))
+ '(org-level-7 ((t (:height 1.0))))
+ '(org-level-8 ((t (:height 1.0)))))
